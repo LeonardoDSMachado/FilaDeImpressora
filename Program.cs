31ae736b7439377d6bd1bba2c3d23f163bec34a5
Leonardo Machado <ldsm.leomachado@gmail.com>
2025-04-26 17:05:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Queue<string> filaImpressora = new Queue<string>();
+
+filaImpressora.Enqueue("Documento em PDF");
+filaImpressora.Enqueue("Planilha de Excel");
+filaImpressora.Enqueue("Calendário");
+filaImpressora.Enqueue("Folha de pagamento");
+
+int itensImpressora = filaImpressora.Count;
+
+Random tempoImpressao = new Random(); 
+
+for (int i = 0; i < itensImpressora; i++)
+{
+    if (filaImpressora.Count > 0)
+    {
+        Console.WriteLine($"imprimindo o arquivo {filaImpressora.Peek()}...\n");
+         
+            int tempoEspera = tempoImpressao.Next(1000, 5001);
+            Thread.Sleep(tempoEspera);
+            Console.WriteLine($"O arquivo {filaImpressora.Dequeue()} foi impresso com sucesso...\n");
+    }
+}
+
+Console.ReadLine();
